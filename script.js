98eaf0a3b3f005057916fb835aa734b0fd74fad4
olivier sedzikowski <oliviersedzikowski@gmail.com>
2023-08-19 14:25:36 +0100

    level100

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
 //You can edit ALL of the code here
 function setup() {
   const allEpisodes = getAllEpisodes();
-  makePageForEpisodes(allEpisodes);
+  makePageForEpisodes(allEpisodes); // cal the function makePageForEpisodes
 }
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
   rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  function cardFunction(value, index, array){
+  const card = document.createElement("div");
+  const epName = document.createElement("h3");
+  const mediumImg = document.createElement("img");
+  const summary = document.createElement("p");
+  rootElem.appendChild(card);
+  card.appendChild(epName);
+  card.appendChild(mediumImg);
+  card.appendChild(summary);
+  card.setAttribute("class", "card");
+  mediumImg.setAttribute("class", "img");
+  epName.textContent = value.name + " - " + "S0" + value.season + "E" + value.number.toString().padStart(2, '0');
+  //.toString().padStart(5, '0')
+  mediumImg.src = value.image.medium
+  summary.innerHTML = value.summary;
+}
+  episodeList.forEach(cardFunction);
 }
 
 window.onload = setup;
